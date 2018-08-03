d4f066d78a8dba28e41e1cc97945d53659ce62ef
Jeffrey Dederick <swift@Jeffreys-MBP.lan1>
2018-08-02 21:04:15 -0400

    initial mvc & test added

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,27 @@
 //jshint strict: false
 module.exports = function(config) {
   config.set({
-
-    basePath: './app',
+    basePath: "./app",
 
     files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      '**/*.module.js',
-      '*!(.module|.spec).js',
-      '!(bower_components)/**/*!(.module|.spec).js',
-      '**/*.spec.js'
+      "bower_components/angular/angular.js",
+      "bower_components/angular-mocks/angular-mocks.js",
+      "**/*.module.js",
+      "*!(.module|.spec).js",
+      "!(bower_components)/**/*!(.module|.spec).js",
+      "**/*.spec.js"
     ],
 
     autoWatch: true,
 
-    frameworks: ['jasmine'],
+    frameworks: ["jasmine"],
 
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ["Chrome"],
 
     plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine'
+      "karma-chrome-launcher",
+      "karma-firefox-launcher",
+      "karma-jasmine"
     ]
-
   });
 };
