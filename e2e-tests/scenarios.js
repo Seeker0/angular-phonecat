697c23d70b720590d16192f5c298a5c9d41cea16
Jeffrey Dederick <swift@mlauriciaw10.kmbsus.local>
2018-08-06 17:52:00 -0400

    added templates, imgs, links and testing

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -8,20 +8,16 @@ describe("PhoneCat Application", function() {
     beforeEach(function() {
       browser.get("index.html");
     });
-    it("should access the phoneList", function() {
-      var phoneList = element.all(by.repeater("phone in $ctrl.phones"));
 
-      expect(phoneList.count()).toBe(3);
-    });
     it("should filter the phone list as a user types into the search box", function() {
       var phoneList = element.all(by.repeater("phone in $ctrl.phones"));
       var query = element(by.model("$ctrl.query"));
-      expect(phoneList.count()).toBe(3);
+      expect(phoneList.count()).toBe(20);
       query.sendKeys("nexus");
       expect(phoneList.count()).toBe(1);
       query.clear();
       query.sendKeys("motorola");
-      expect(phoneList.count()).toBe(2);
+      expect(phoneList.count()).toBe(8);
     });
 
     it("should be possible to control the phone order via the drop-down menu", function() {
@@ -51,6 +47,17 @@ describe("PhoneCat Application", function() {
         "MOTOROLA XOOM\u2122",
         "Motorola XOOM\u2122 with Wi-Fi"
       ]);
+    });
+
+    it("should render phone specific links", function() {
+      var query = element(by.model("$ctrl.query"));
+      query.sendKeys("nexus");
+
+      element
+        .all(by.css(".phones li a"))
+        .first()
+        .click();
+      expect(browser.getLocationAbsUrl()).toBe("/phones/nexus-s");
     });
   });
 });
