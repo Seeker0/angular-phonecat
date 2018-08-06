7c90dbf069a226ecb3549b7bfda7c8d1774a355f
Jeffrey Dederick <swift@Jeffreys-MBP.lan1>
2018-08-06 15:36:04 -0400

    added sorting and 2way binding, tested

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -8,15 +8,18 @@ angular.module("phoneList").component("phoneList", {
     this.phones = [
       {
         name: "Nexus S",
-        snippet: "Fast just got faster with Nexus S."
+        snippet: "Fast just got faster with Nexus S.",
+        age: 1
       },
       {
-        name: "Motorola XOOM with Wi-Fi",
-        snippet: "The Next, Next Generation tablet."
+        name: "Motorola XOOM\u2122 with Wi-Fi",
+        snippet: "The Next, Next Generation tablet.",
+        age: 2
       },
       {
-        name: "MOTOROLA XOOM",
-        snippet: "The Next, Next Generatio tablet."
+        name: "MOTOROLA XOOM\u2122",
+        snippet: "The Next, Next Generatio tablet.",
+        age: 3
       }
     ];
   }
